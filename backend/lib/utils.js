553a8866903095981854e56a1768e198f6672155
Sibusiso Matebese <sibusisomatebese75@gmail.com>
2025-10-06 18:01:49 +0200

    https

--- backend/lib/utils.js
+++ backend/lib/utils.js
@@ -12,10 +12,10 @@ export const generateToken = (userId, res) => {
   });
 
   res.cookie("jwt", token, {
-    httpOnly: true,
-  sameSite: ENV.NODE_ENV === "production" ? "none" : "lax",
-  secure: ENV.NODE_ENV === "production",
-  maxAge: 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
+    httpOnly: true, // prevent XSS attacks: cross-site scripting
+    sameSite: "strict", // CSRF attacks
+    secure:true,
   });
 
   return token;
