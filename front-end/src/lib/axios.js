1f121cb86c94434b67e624a637172ce8ccaf62f7
Sibusiso Matebese <sibusisomatebese75@gmail.com>
2025-10-06 18:11:41 +0200

    baseURL

--- front-end/src/lib/axios.js
+++ front-end/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://chat-application-back-end-3q12.onrender.com/api" : "/api",
+  baseURL:"https://chat-application-back-end-3q12.onrender.com/api",
   withCredentials: true,
 });
